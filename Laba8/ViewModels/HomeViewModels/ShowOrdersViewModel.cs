c49dabf9ec3edce4e16e0e1fc3a1f1712aebdd84
IlIEmptyIlI <132489781+IlIEmptyIlI@users.noreply.github.com>
2023-11-13 12:25:27 +0200

    Add files via upload

---
+++ Laba8/ViewModels/HomeViewModels/ShowOrdersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Laba8.Models;
+
+namespace Laba8.ViewModels.HomeViewModels
+{
+    public enum ShowStyles
+    {
+        List,
+        Table
+    }
+    public record class ShowOrdersViewModel(IEnumerable<Order> Orders, ShowStyles ShowStyle);
+}
